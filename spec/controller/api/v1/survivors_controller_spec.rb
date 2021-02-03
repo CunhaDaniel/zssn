1fc76e6bd5236d11d3627d8e5bb8412282d0bba6
CunhaDaniel <daniel.meskita1997@gmail.com>
2021-02-03 20:06:24 -0300

    news test add

--- spec/controller/api/v1/survivors_controller_spec.rb
+++ spec/controller/api/v1/survivors_controller_spec.rb
@@ -5,15 +5,29 @@ RSpec.describe Api::V1::SurvivorsController, type: :controller do
   let!(:survivor1) { create(:survivor) }
   let!(:survivor2) { create(:survivor) }
 
-  
-  describe '#survivor' do
-    it 'GET#index' do
-      get :index
-  
+  describe 'GET#show' do
+    before { get :show, params: { id: survivor.id } }
 
+    it 'returns http success' do
       expect(response).to have_http_status(:success)
-      binding.pry
+    end
+
+    it 'response containing expected survivor' do
+      @test_survivor = Survivor.find_by(id: JSON.parse(response.body)['id'])
+      expect(@test_survivor).to eql(survivor)
     end
   end
 
+  describe 'GET#index' do
+    before { get :index }
+
+    it 'returns http sucess' do
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'expect to 3 survivors' do
+      expect(JSON.parse(response.body).length).to eql(3)
+    end
+  end
 end
+
